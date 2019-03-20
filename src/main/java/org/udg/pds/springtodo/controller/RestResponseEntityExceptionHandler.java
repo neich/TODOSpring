0c6ac89e83c00fa6cf6890d0ae04e5e1ef49ccf9
Ignacio Martín <natx.mc@gmail.com>
2019-03-20 16:32:55 +0100

    Refactor class to account for all types of exceptions

--- src/main/java/org/udg/pds/springtodo/controller/RestResponseEntityExceptionHandler.java
+++ src/main/java/org/udg/pds/springtodo/controller/RestResponseEntityExceptionHandler.java
@@ -1,26 +1,23 @@
 package org.udg.pds.springtodo.controller;
 
-import org.springframework.beans.ConversionNotSupportedException;
-import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.http.converter.HttpMessageNotWritableException;
-import org.springframework.validation.BindException;
-import org.springframework.web.HttpMediaTypeNotAcceptableException;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.MissingPathVariableException;
-import org.springframework.web.bind.MissingServletRequestParameterException;
-import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
-import org.springframework.web.multipart.support.MissingServletRequestPartException;
-import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.udg.pds.springtodo.Global;
+import org.udg.pds.springtodo.controller.exceptions.ControllerException;
+import org.udg.pds.springtodo.controller.exceptions.ServiceException;
+import org.udg.pds.springtodo.entity.Error;
+
+import java.util.Date;
 
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
@@ -29,84 +26,64 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
         super();
     }
 
-    @Override
-    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        return super.handleHttpRequestMethodNotSupported(ex, headers, status, request);
-    }
-
-    @Override
-    protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(HttpMediaTypeNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        return super.handleHttpMediaTypeNotSupported(ex, headers, status, request);
-    }
-
-    @Override
-    protected ResponseEntity<Object> handleHttpMediaTypeNotAcceptable(HttpMediaTypeNotAcceptableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        return super.handleHttpMediaTypeNotAcceptable(ex, headers, status, request);
-    }
-
-    @Override
-    protected ResponseEntity<Object> handleMissingPathVariable(MissingPathVariableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        return super.handleMissingPathVariable(ex, headers, status, request);
-    }
-
-    @Override
-    protected ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        return super.handleMissingServletRequestParameter(ex, headers, status, request);
-    }
-
-    @Override
-    protected ResponseEntity<Object> handleServletRequestBindingException(ServletRequestBindingException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        return super.handleServletRequestBindingException(ex, headers, status, request);
-    }
-
-    @Override
-    protected ResponseEntity<Object> handleConversionNotSupported(ConversionNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        return super.handleConversionNotSupported(ex, headers, status, request);
-    }
-
-    @Override
-    protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        return super.handleTypeMismatch(ex, headers, status, request);
-    }
-
-    @Override
-    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        return super.handleHttpMessageNotReadable(ex, headers, status, request);
-    }
-
-    @Override
-    protected ResponseEntity<Object> handleHttpMessageNotWritable(HttpMessageNotWritableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        return super.handleHttpMessageNotWritable(ex, headers, status, request);
-    }
+    // Overriding of predefined methods in ResponseEntityExceptionHandler
+    // NOT ALL are overrided. Look ResponseEntityExceptionHandler
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
                                                                   HttpHeaders headers, HttpStatus status, WebRequest request) {
-        return new ResponseEntity("jjaaaarrrllll", HttpStatus.BAD_REQUEST);
+        StringBuilder sb = new StringBuilder("Errors: ");
+        for (ObjectError error : ex.getBindingResult().getAllErrors()) {
+            FieldError fe = (FieldError) error;
+            if (fe != null) sb.append(String.format("[Field: %s, error: %s ] ", fe.getField(), fe.getDefaultMessage()));
+        }
+
+        return handleExceptionInternal(ex,
+                new Error(Global.dateFormat.format(new Date()),
+                        status.value(),
+                        "Validation failed for body",
+                        sb.toString()),
+                headers, status, request);
     }
 
     @Override
-    protected ResponseEntity<Object> handleMissingServletRequestPart(MissingServletRequestPartException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        return super.handleMissingServletRequestPart(ex, headers, status, request);
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(
+            HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        return handleExceptionInternal(ex,
+                new Error(Global.dateFormat.format(new Date()),
+                        status.value(),
+                        "Message not readable",
+                        ex.getMessage()),
+                headers, status, request);
     }
 
-    @Override
-    protected ResponseEntity<Object> handleBindException(BindException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        return super.handleBindException(ex, headers, status, request);
+    // Other exceptions. Add your own exception handling here
+
+    @ExceptionHandler({ Exception.class })
+    protected ResponseEntity<Object> handleOtherExceptions(Exception ex, WebRequest request) {
+
+        if (ex instanceof ServiceException) {
+            return handleExceptionInternal(ex,
+                    new Error(Global.dateFormat.format(new Date()),
+                            HttpStatus.BAD_REQUEST.value(),
+                            "Service error",
+                            ex.getMessage()),
+                    new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+        } else if (ex instanceof ControllerException) {
+            return handleExceptionInternal(ex,
+                    new Error(Global.dateFormat.format(new Date()),
+                            HttpStatus.BAD_REQUEST.value(),
+                            "Controller error",
+                            ex.getMessage()),
+                    new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+        } else
+            return handleExceptionInternal(ex,
+                new Error(Global.dateFormat.format(new Date()),
+                        HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                        "Unknown error",
+                        ex.getMessage()),
+                new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
     }
 
-    @Override
-    protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        return super.handleNoHandlerFoundException(ex, headers, status, request);
-    }
-
-    @Override
-    protected ResponseEntity<Object> handleAsyncRequestTimeoutException(AsyncRequestTimeoutException ex, HttpHeaders headers, HttpStatus status, WebRequest webRequest) {
-        return super.handleAsyncRequestTimeoutException(ex, headers, status, webRequest);
-    }
-
-    @Override
-    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        return super.handleExceptionInternal(ex, body, headers, status, request);
-    }
 }
