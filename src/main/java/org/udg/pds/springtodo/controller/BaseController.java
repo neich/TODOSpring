093bd7a274618c7cbf4d87b1c9af649f99ad6b5b
Ignacio Martín <natx.mc@gmail.com>
2022-01-25 13:15:04 +0100

    Reformat code

--- src/main/java/org/udg/pds/springtodo/controller/BaseController.java
+++ src/main/java/org/udg/pds/springtodo/controller/BaseController.java
@@ -3,49 +3,46 @@ package org.udg.pds.springtodo.controller;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.udg.pds.springtodo.controller.exceptions.ControllerException;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-
-import java.io.IOException;
 
 /**
  * Created by imartin on 21/02/17.
  */
 public class BaseController {
 
-  static String OK_MESSAGE = "\"ok\"";
+    static String OK_MESSAGE = "\"ok\"";
 
-  Long getLoggedUser(HttpSession session) {
+    Long getLoggedUser(HttpSession session) {
 
-    if (session == null) {
-      throw new ControllerException("No sessions available!");
+        if (session == null) {
+            throw new ControllerException("No sessions available!");
+        }
+
+        Long userId = (Long) session.getAttribute("simpleapp_auth_id");
+        // Check if the session has the attribute "simpleapp_auth_id"
+        if (userId == null)
+            throw new ControllerException("User is not authenticated!");
+
+        return userId;
     }
 
-    Long userId = (Long)session.getAttribute("simpleapp_auth_id");
-    // Check if the session has the attribute "simpleapp_auth_id"
-    if (userId == null)
-      throw new ControllerException("User is not authenticated!");
+    void checkNotLoggedIn(HttpSession session) {
+        // Access to the HTTP session
 
-    return userId;
-  }
+        if (session == null) {
+            throw new ControllerException("No sessions available!");
+        }
 
-  void checkNotLoggedIn(HttpSession session) {
-    // Access to the HTTP session
-
-    if (session == null) {
-      throw new ControllerException("No sessions available!");
+        Long userId = (Long) session.getAttribute("simpleapp_auth_id");
+        // Check if the session has the attribute "simpleapp_auth_id"
+        if (userId != null)
+            throw new ControllerException("User is already authenticated!");
     }
 
-    Long userId = (Long)session.getAttribute("simpleapp_auth_id");
-    // Check if the session has the attribute "simpleapp_auth_id"
-    if (userId != null)
-      throw new ControllerException("User is already authenticated!");
-  }
-
-  MappingJacksonValue toResponse(Object pojo, Class<?> view) {
-    final MappingJacksonValue result = new MappingJacksonValue(pojo);
-    result.setSerializationView(view);
-    return result;
-  }
+    MappingJacksonValue toResponse(Object pojo, Class<?> view) {
+        final MappingJacksonValue result = new MappingJacksonValue(pojo);
+        result.setSerializationView(view);
+        return result;
+    }
 
 }
