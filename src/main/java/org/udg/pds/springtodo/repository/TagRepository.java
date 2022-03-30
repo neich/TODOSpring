687d1dcad8839b41b664e0b7ae819d3953c8b1bd
Ignacio Martín <natx.mc@gmail.com>
2022-03-30 15:23:20 +0200

    Convert repositories to JpaRepository to use derived queries

--- src/main/java/org/udg/pds/springtodo/repository/TagRepository.java
+++ src/main/java/org/udg/pds/springtodo/repository/TagRepository.java
@@ -1,9 +1,9 @@
 package org.udg.pds.springtodo.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 import org.udg.pds.springtodo.entity.Tag;
 
 @Component
-public interface TagRepository extends CrudRepository<Tag, Long> {
+public interface TagRepository extends JpaRepository<Tag, Long> {
 }
--- src/main/java/org/udg/pds/springtodo/repository/TaskRepository.java
+++ src/main/java/org/udg/pds/springtodo/repository/TaskRepository.java
@@ -1,9 +1,9 @@
 package org.udg.pds.springtodo.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 import org.udg.pds.springtodo.entity.Task;
 
 @Component
-public interface TaskRepository extends CrudRepository<Task, Long> {
+public interface TaskRepository extends JpaRepository<Task, Long> {
 }
