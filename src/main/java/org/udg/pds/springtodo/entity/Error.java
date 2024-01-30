6fe6b34eb648cbb358d826a97f47b5e564354e97
Ignacio Martin <natx.mc@gmail.com>
2024-01-30 11:27:44 +0100

    Added @Getter

--- src/main/java/org/udg/pds/springtodo/entity/Error.java
+++ src/main/java/org/udg/pds/springtodo/entity/Error.java
@@ -1,17 +1,22 @@
 package org.udg.pds.springtodo.entity;
-
 import lombok.Data;
+import lombok.Getter;
 import lombok.NonNull;
 
 @Data
 public class Error {
     String path;
     @NonNull
+    @Getter
     private String timestamp;
     @NonNull
+    @Getter
     private Integer status;
     @NonNull
+    @Getter
     private String error;
     @NonNull
+    @Getter
     private String message;
+
 }
