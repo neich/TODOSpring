e661071d7d677ed7908cdf1e6b94db64df94394e
Ignacio Martin <natx.mc@gmail.com>
2024-05-13 13:56:52 +0200

    Add lombok support. Forbid id setter usage

--- src/main/java/org/udg/pds/springtodo/entity/BaseEntity.java
+++ src/main/java/org/udg/pds/springtodo/entity/BaseEntity.java
@@ -5,15 +5,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
 
 @MappedSuperclass
+@Data
 public class BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
     protected Long id;
-
-    public Long getId() {
-        return id;
-    }
 }
