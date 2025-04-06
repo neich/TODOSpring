9ec53e2ebcfc9ec4ae534ff6bda9a8db517a8bb4
Ignacio Martin <natx.mc@gmail.com>
2025-04-06 19:02:11 +0200

    Move method with parameters ins the path to the end of the class

--- src/main/java/org/udg/pds/springtodo/controller/UserController.java
+++ src/main/java/org/udg/pds/springtodo/controller/UserController.java
@@ -43,29 +43,6 @@ public class UserController extends BaseController {
         return ResponseEntity.noContent().build();
     }
 
-    @GetMapping(path = "/{id}")
-    public UserDto getPublicUser(HttpSession session, @PathVariable("id") @Valid Long userId) {
-
-        getLoggedUser(session);
-
-        return userMapper.userToUserDto(userService.getUser(userId));
-    }
-
-    @DeleteMapping(path = "/{id}")
-    public ResponseEntity<Void> deleteUser(HttpSession session, @PathVariable("id") Long userId) {
-
-        Long loggedUserId = getLoggedUser(session);
-
-        if (!loggedUserId.equals(userId))
-            throw new ControllerException("You cannot delete other users!");
-
-        userService.deleteUser(userId);
-        session.removeAttribute("simpleapp_auth_id");
-
-        return ResponseEntity.noContent().build();
-    }
-
-
     @PostMapping(path = "/register", consumes = "application/json")
     public UserDto register(HttpSession session, @Valid @RequestBody RegisterUser ru) {
 
@@ -90,6 +67,29 @@ public class UserController extends BaseController {
 
         return ResponseEntity.noContent().build();
     }
+
+    @GetMapping(path = "/{id}")
+    public UserDto getPublicUser(HttpSession session, @PathVariable("id") @Valid Long userId) {
+
+        getLoggedUser(session);
+
+        return userMapper.userToUserDto(userService.getUser(userId));
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> deleteUser(HttpSession session, @PathVariable("id") Long userId) {
+
+        Long loggedUserId = getLoggedUser(session);
+
+        if (!loggedUserId.equals(userId))
+            throw new ControllerException("You cannot delete other users!");
+
+        userService.deleteUser(userId);
+        session.removeAttribute("simpleapp_auth_id");
+
+        return ResponseEntity.noContent().build();
+    }
+
 
     private static class LoginUser {
 
